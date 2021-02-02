0d538a5a8aca0aae8ccd7caf943bc75b1c63d050
Nicholas Nicholson <nicholas.nicholson@hcl.com>
2021-02-02 10:39:38 -0500

    first commit

---
+++ src/main/java/com/junit/authentication/UserAuthentication.java
@@ -0,0 +1,17 @@
+package com.junit.authentication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserAuthentication {
+	public static List<> userDatabase = new ArrayList<User>
+	
+	public boolean validatLogin (String username, String password) {
+		boolean loginValid = false;
+		
+		
+		
+		return loginValid;
+	}
+
+}
